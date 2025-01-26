0c1c916c6e1960acb46206604dccdb58a327eaaa
Jose Villa <j0542420@amarillocollege.com>
2025-01-25 19:43:24 -0600

    update

--- Program.cs
+++ Program.cs
@@ -10,6 +10,25 @@ namespace Wk2Ex3
     {
         static void Main(string[] args)
         {
+            //declarations
+            double F = '0';
+
+            //title of the converter app
+            Console.WriteLine("Welcome to Temperature Converter");
+
+            //input the temperature in F
+            Console.WriteLine("Enter the temperature: ");
+            //collect the user input of the temperature
+            F = Convert.ToInt32(Console.ReadLine());
+
+            //calculate the temperature from F to C
+            double C = (F-32)*5/9;
+            // round up to two decimals
+            C = Math.Round(C, 2);
+
+            //output the result
+            Console.WriteLine($"{F}°F is equivalent to {C}°C");
+
         }
     }
 }
